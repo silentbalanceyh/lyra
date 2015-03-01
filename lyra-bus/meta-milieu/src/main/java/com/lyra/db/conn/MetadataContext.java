a6258c37d87131544416d752dcf0c1cf87f5084a
silentbalanceyh <silentbalanceyh@126.com>
2015-03-02 02:18:58 +0800

    meta

--- lyra-bus/meta-milieu/src/main/java/com/lyra/db/conn/MetadataContext.java
+++ lyra-bus/meta-milieu/src/main/java/com/lyra/db/conn/MetadataContext.java
@@ -62,47 +62,4 @@ public interface MetadataContext {
 	String getDatabaseName();
 
 	// endregion
-
-	// region Metadata: Readdata without parameters
-
-	/**
-	 * Execute sql statement
-	 */
-	void execute(final String ddlSql);
-
-	/**
-	 * Read single column value into Set
-	 *
-	 * @param querySql
-	 * @param columnName
-	 * @return
-	 */
-	Set<String> selectColumn(final String querySql, final String columnName);
-
-	/**
-	 * Read single row value into hashMap
-	 *
-	 * @param querySql
-	 * @param columns
-	 * @return
-	 */
-	Map<String, String> selectRow(final String querySql,
-			final String... columns);
-
-	/**
-	 * Read multi rows value into hashMap
-	 *
-	 * @param querySql
-	 * @param columns
-	 * @return
-	 */
-	List<Map<String, String>> selectRows(final String querySql,
-			final String... columns);
-
-	/**
-	 * @param countSql
-	 * @return
-	 */
-	<T extends Number> T count(final String countSql, Class<T> retType);
-	// endregion
 }
--- lyra-bus/meta-milieu/src/main/java/com/lyra/meta/database/Metadata.java
+++ lyra-bus/meta-milieu/src/main/java/com/lyra/meta/database/Metadata.java
@@ -1,8 +1,27 @@
-package com.lyra.db.conn.impl;
-
+package com.lyra.meta.database;
+/**
+ * 数据库元数据
+ *
+ * @author Lang
+ * @see
+ */
 public class Metadata {
 	// ~ Static Fields =======================================
 	// ~ Instance Fields =====================================
+	
+	private String product;
+	
+	private String version;
+	
+	private String driverName;
+	
+	private String driverVersion;
+	
+	private String username;
+	
+	private String databaseCategory;
+	
+	private String databaseName;
 	// ~ Static Block ========================================
 	// ~ Static Methods ======================================
 	// ~ Constructors ========================================
